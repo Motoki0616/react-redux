3ef0d241826ff36f2a13d7bfef7e2653212069f0
Motoki Naito <tonponpon4s@gmail.com>
2017-03-20 21:25:04 -0700

    added action, components,reducers, store

---
+++ client/components/Photo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Photo = () => {
+    return (
+        <figure className="grid-figure">
+            I am a ph!
+        </figure>
+    );
+}
+
+export default Photo;
